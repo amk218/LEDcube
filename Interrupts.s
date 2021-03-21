7ea9d5c7c276299773c233a31ce16602ff0f3a21
Incandus <77331190+Incandus@users.noreply.github.com>
2021-03-21 19:12:59 +0000

    Interrupts
    Added button interrupt for the first time - likely errors.
    main script beginning to flesh out to run the loops - pattern look up table required

--- Interrupts.s
+++ Interrupts.s
@@ -1,8 +1,11 @@
 #include <xc.inc>
 	
-global	DAC_Setup, DAC_Int_Hi, High_priority_interrupt, Low_priority_interrupt, Interrupt_setup
+global	High_priority_interrupt, Interrupt_setup
+extrn	pattern_counter, pattern_select
     
 psect	dac_code, class=CODE
+    
+
 	
 DAC_Int_Hi:	
 	btfss	TMR0IF		; check that this is timer0 interrupt
@@ -11,31 +14,32 @@ DAC_Int_Hi:
 	bcf	TMR0IF		; clear interrupt flag
 	retfie	f		; fast return from interrupt
 
-DAC_Setup:
-	clrf	TRISJ, A	; Set PORTD as all outputs
-	clrf	LATJ, A		; Clear PORTD outputs
-	movlw	10000111B	; Set timer0 to 16-bit, Fosc/4/256
-	movwf	T0CON, A	; = 62.5KHz clock rate, approx 1sec rollover
-	bsf	TMR0IE		; Enable timer0 interrupt
-	bsf	GIE		; Enable all interrupts
-	return
+
 	
+Interrupt_setup:
+	bsf	GIE		; enable globale interrupts
+	bsf	RBIE		; enable interrrupt on port B4:7 change state
+	bcf	RBPU		; disable PORTB pull ups
+	bsf	RBIP		; set it to be high priority
+	return
 
     
     
     
 High_priority_interrupt:
-    ;check for button press
-    ;go to change pattern
+    btfss   PORTB, 5, A
+    bra	    change_pattern
     ;check for light sensor
     ;go to light override
-    ; reset flags
-    ; return to main program
+
+    retfie  f
 
 
     
-Change_pattern:			; High priority interupt that will change the pattern cycle on button press
-    ; +1 to pattern counter
-    ; call pattern lookup
-    ; put WREG into FSR1, FSR2
-    ; return
+Change_pattern:			    ; High priority interupt that will change the pattern cycle on button press
+    pop				    ; remove return from stack
+    bcf	    RBIF		    ; clear the interrupt flag
+    decfsz  pattern_counter	    ; change pattern counter (i.e. pattern)
+    goto    pattern_select	    ; select new pattern
+    movf    pattern_number, W, A    ; if pattern number is maxed then reset counter
+    goto    pattern_select	    ; select new pattern after counter reset
--- main.s
+++ main.s
@@ -1,16 +1,23 @@
 #include <xc.inc>
 
-extrn	DAC_Int_Hi, DAC_Setup
+extrn	Interrupt_setup, High_priority_interrupt
 extrn	High_priority_interrupt, Low_priority_interrupt, Interrupt_setup ; From interrupt.s
 extrn	load_to_RAM ; From Pattern_table.s	
 extrn	single_layer_test, layer_by_layer, static_output ; From Patterns.s		
-	
 
+global	pattern_counter    
+    
+psect	udata_acs   ; reserve data space in access ram
+pattern_counter:    ds 1    ; reserve one byte for a counter variable 
+pattern_number EQU  6	    ; This is the number of available patterns (THIS MUST BE UPDATED WITH ANY NEW PATTERN)
+    
 psect	code, abs
 
 	org	0x0000
 	goto	setup
 	
+	org	0x0008
+	goto	
 
 	
 psect	code, abs
@@ -21,20 +28,24 @@ setup:			    ; Set ports D-F as outputs and clear them
 	clrf	LATH, A
 	clrf	TRISE, A
 	clrf	LATE, A
+	movlw	0b11111111
+	movwf	TRISB, A
 	call	load_to_RAM
-	;call	Interupt_setup
-    Load_initial_config:
-	; FSR1, FSR2 = 0x400
-	; set pattern counter to 0
+	call	Interupt_setup
+	movlw	pattern_number
+	movwf	pattern_counter
+
 	goto	start
 
 
 
 	
 start:
-	lfsr	1, 0x400
-	call	static_output
-
+	;call	light_sensor_loop
+	movlw	pattern_counter
+	addwf	W,W,A
+	addwf	PCL, F, A
+	   ; Lookup table goes here
 
 	
 	bra	$
